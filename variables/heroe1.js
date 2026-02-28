32e6ee190bae4a8d1033f765fafbdc064fc55662
KrowZy34 <leobastian562@gmail.com>
2026-02-28 13:43:23 -0500

    chore: hero 2

---
+++ variables/heroe1.js
@@ -0,0 +1,6 @@
+let nombreHeroe = "Spiderman";
+let poder = "trepar muros";
+let nivelEnergia = 100;
+let ciudad = "New York";
+let enemigo = "Venom";
+console.log(nombreHeroe + " esta a " + nivelEnergia + " de energia Y se esta defendido la ciudad de : " + ciudad + " del villano " + enemigo);
---
+++ variables/heroe2.js
@@ -0,0 +1,7 @@
+let nombreHeroe = "Hulk";
+let poder = "super fuerza";
+let nivelEnergia = 150;
+let ciudad = "New York";
+let enemigo = "Abominación";
+
+console.log(`${nombreHeroe} tiene como poder la ${poder}, se encuentra peleando contra ${enemigo} en la ciodad de ${ciudad} y tiene ${nivelEnergia} de energia.`);
